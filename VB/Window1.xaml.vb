e9b0641a7ea7bd11c880a9f6249d150197d26603
DevExpressExampleBot <fakeEmail>
2022-12-17 08:23:24 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,39 +1,34 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Xpf.Bars
 Imports DevExpress.Xpf.Grid
 
 Namespace ShowGridMenu
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-	   #Region "#ShowGridMenu"
-		Private Sub TableView_ShowGridMenu(ByVal sender As Object, ByVal e As GridMenuEventArgs)
-			' Check whether this event was raised for a column's context menu.
-			If e.MenuType <> GridMenuType.Column Then
-				Return
-			End If
+'#Region "#ShowGridMenu"
+        Private Sub TableView_ShowGridMenu(ByVal sender As Object, ByVal e As GridMenuEventArgs)
+            ' Check whether this event was raised for a column's context menu.
+            If e.MenuType <> GridMenuType.Column Then Return
+            ' Remove the Column Chooser menu item.
+            e.Customizations.Add(New RemoveBarItemAndLinkAction() With {.ItemName = DefaultColumnMenuItemNamesBase.ColumnChooser})
+            ' Create a custom menu item and add it to the context menu.
+            Dim bi As BarButtonItem = New BarButtonItem()
+            bi.Name = "customItem"
+            bi.Content = "Custom Item"
+            AddHandler bi.ItemClick, New ItemClickEventHandler(AddressOf customItem_ItemClick)
+            e.Customizations.Add(bi)
+        End Sub
 
-			' Remove the Column Chooser menu item.
-			e.Customizations.Add(New RemoveBarItemAndLinkAction() With {.ItemName = DefaultColumnMenuItemNames.ColumnChooser})
-
-			' Create a custom menu item and add it to the context menu.
-			Dim bi As New BarButtonItem()
-			bi.Name = "customItem"
-			bi.Content = "Custom Item"
-			AddHandler bi.ItemClick, AddressOf customItem_ItemClick
-			e.Customizations.Add(bi)
-		End Sub
-
-		Private Sub customItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-			' Implement the custom action.
-			' ...
-		End Sub
-	   #End Region ' #ShowGridMenu
-
-	End Class
+        Private Sub customItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+        ' Implement the custom action.
+        ' ...
+        End Sub
+'#End Region  ' #ShowGridMenu
+    End Class
 End Namespace
